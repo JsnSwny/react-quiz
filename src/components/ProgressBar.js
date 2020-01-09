4c7a10234f81a9397236b020d245c902a8b6e7f4
Jason Sweeney <jsnswny@gmail.com>
2020-01-09 23:03:52 +0000

    Added percentage to progress bar

--- src/components/ProgressBar.js
+++ src/components/ProgressBar.js
@@ -4,7 +4,9 @@ export class ProgressBar extends Component {
     render() {
         return (
             <div style={{ width: "100%",  height: "30px" }} className="bg-gray-800 mt-4 rounded">
-                <div style={{ width: this.props.percentage, height: "100%" }} className="bg-green-200 rounded-l"></div>
+                <div style={{ width: "100%" }} className="text-center z-50 text-white font-bold absolute mt-1">{this.props.percentage}</div>
+                <div style={{ width: this.props.percentage, height: "100%" }} className="bg-green-600 rounded-l"></div>
+                
             </div>
         )
     }
--- src/components/Question.js
+++ src/components/Question.js
@@ -8,7 +8,7 @@ export class Question extends Component {
             <React.Fragment>
                 <h1 className="text-xl md:text-4xl bg-gray-900 text-white w-3/4 mx-auto p-4 text-center font-bold uppercase rounded-lg">Question { questionNumber + 1 }</h1>
                 <ProgressBar 
-                    percentage={ ((questionNumber+1) / questions.length) * 100 + "%"}
+                    percentage={ (((questionNumber+1) / questions.length) * 100).toFixed(0) + "%"}
                 />
                 <div className="bg-gray-300 p-8 mt-4 mx-auto shadow-md">
                     <p className="text-center lg:text-left text-lg sm:text-xl lg:text-2xl font-bold">{ questions[questionNumber].question }</p>
